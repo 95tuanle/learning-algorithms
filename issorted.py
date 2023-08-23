0f9f2857e6bba182529280f8e4fc147208843051
Tuan Le <tlna95@icloud.com>
2023-08-22 20:04:13 -0400

    shorten

--- issorted.py
+++ issorted.py
@@ -3,10 +3,11 @@ items2 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
 
 
 def is_sorted(item_list):
-    for i in range(len(item_list) - 1):
-        if item_list[i] > item_list[i + 1]:
-            return False
-    return True
+    # for i in range(len(item_list) - 1):
+    #     if item_list[i] > item_list[i + 1]:
+    #         return False
+    # return True
+    return all(item_list[i] <= item_list[i + 1] for i in range(len(item_list) - 1))
 
 
 print(is_sorted(items1))
